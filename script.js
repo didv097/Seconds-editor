314212fe09b58e3f88bd6ea7b75b6be3116b2af9
bran0 <bran107@outlook.com>
2019-11-18 13:17:40 -0500

    commit

--- script.js
+++ script.js
@@ -14,6 +14,14 @@ let btn_forward10 = document.getElementById("btn-forward10s");
 let btn_pause = document.getElementById("btn-pause");
 let opt_exercises = document.getElementById("opt-exercises");
 let btn_match = document.getElementById("btn-match");
+let input_start_time = document.getElementById("input-start-time");
+let input_duration = document.getElementById("input-duration");
+let input_end_time = document.getElementById("input-end-time");
+let text_exercise = document.getElementById("text-exercise");
+let btn_previous = document.getElementById("btn-previous");
+let btn_next = document.getElementById("btn-next");
+let btn_save = document.getElementById("btn-save");
+let btn_save_all = document.getElementById("btn-save-all");
 
 let audio;
 let workouts = []; // id, name, mp3, live_data
@@ -21,6 +29,8 @@ let intervals = [];	// type, name, duration, start, end
 let exercises = []; // name, id
 let selectedWorkout = -1;
 let dur_audio = 0, cur_time = 0;
+let cur_interval = 0, cur_exercise = 0;
+let cur_start_time = 0, cur_duration = 0, cur_end_time = 0;
 input_csv.onchange = () => {
 	opt_workout.length = 0;
 	workouts = [];
@@ -63,8 +73,8 @@ xHttpCSV.addEventListener("load", () => {
 		}
 	}
 });
-xHttpCSV.send(null)
-btn_process.onclick = event => {
+xHttpCSV.send(null);
+function btnProcessClicked() {
 	selectedIndex = opt_workout.selectedIndex;
 	if (selectedIndex < 0) {
 		return;
@@ -107,13 +117,20 @@ btn_process.onclick = event => {
 					elem_interval.classList = ["exercise"];
 					// elem_interval.classList.add("intro");
 					elem_interval.style.width = (intervals[i].duration / dur_audio * 100) + "%";
-					console.log(elem_interval.style.width)
 					let elem_icon = document.createElement("span");
 					elem_icon.className = "exer-icon";
 					elem_icon.innerHTML = "&#x1f5d1;";
+					elem_icon.addEventListener("click", () => {
+						/* remove interval */
+					})
 					let elem_name = document.createElement("span");
 					elem_name.className = "exer-name";
 					elem_name.innerHTML = intervals[i].name;
+					elem_name.addEventListener("click", () => {
+						cur_interval = i;
+						cur_time = intervals[cur_interval].start_time;
+						audio.currentTime = cur_time;
+					})
 					elem_interval.appendChild(elem_icon);
 					elem_interval.appendChild(elem_name);
 					cont_exercises.appendChild(elem_interval);
@@ -122,18 +139,38 @@ btn_process.onclick = event => {
 		})
 		xHttp.send(null);
 	});
+	audio.addEventListener("timeupdate", () => {
+		cur_time = audio.currentTime;
+		elem_cur_time.innerText = ("00" + parseInt(cur_time / 60).toString()).slice(-2)
+			+ ":" + ("00" + parseInt(cur_time % 60).toString()).slice(-2);
+		progressbar.style.width = (100 * cur_time / dur_audio) + "%";
+		for (let i = 0; i < intervals.length; i ++) {
+			if (cur_time < intervals[i].end_time) {
+				cur_interval = i;
+				break;
+			}
+		}
+		cur_start_time = intervals[cur_interval].start_time;
+		cur_duration = intervals[cur_interval].duration;
+		cur_end_time = intervals[cur_interval].end_time;
+		input_start_time.value = cur_start_time;
+		input_duration.value = cur_duration;
+		input_end_time.value = cur_end_time;
+		text_exercise.innerText = intervals[cur_interval].name;
+	});
 }
-audio.addEventListener("timeupdate", () => {
-	cur_time = audio.currentTime;
-	elem_cur_time.innerText = ("00" + parseInt(cur_time / 60).toString()).slice(-2)
-		+ ":" + ("00" + parseInt(cur_time % 60).toString()).slice(-2);
-	progressbar.style.width = (100 * cur_time / dur_audio) + "%";
-});
+btn_process.addEventListener("click", () => btnProcessClicked());
 btn_play.addEventListener("click", () => {
 	audio.play();
+	input_start_time.readOnly = true;
+	// input_duration.readOnly = true;
+	input_end_time.readOnly = true;
 });
 btn_pause.addEventListener("click", () => {
 	audio.pause();
+	input_start_time.readOnly = false;
+	// input_duration.readOnly = false;
+	input_end_time.readOnly = false;
 });
 btn_back10.addEventListener("click", () => {
 	cur_time -= 10;
@@ -146,5 +183,77 @@ btn_forward10.addEventListener("click", () => {
 	audio.currentTime = cur_time;
 });
 btn_match.addEventListener("click", () => {
-	
-})
+	cur_exercise = -1;
+	for (let i = 0; i < exercises.length; i ++) {
+		if (exercises[i].name == intervals[cur_interval].name) {
+			cur_exercise = i;
+			break;
+		}
+	}
+	if (cur_exercise < 0) {
+		exercises.push({
+			name: intervals[cur_interval].name,
+			id: intervals[cur_interval].name
+		});
+		let opt = document.createElement("option");
+		opt.text = intervals[cur_interval].name;
+		opt_exercises.add(opt);
+		cur_exercise = exercises.length - 1;
+		opt_exercises.value = intervals[cur_interval].name;
+	} else {
+		opt_exercises.value = exercises[cur_exercise].name;
+	}
+});
+btn_previous.addEventListener("click", () => {
+	if (selectedIndex > 0) {
+		opt_workout.selectedIndex --;
+		btnProcessClicked();
+	}
+});
+btn_next.addEventListener("click", () => {
+	if (selectedIndex < workouts.length - 1) {
+		opt_workout.selectedIndex ++;
+		btnProcessClicked();
+	}
+});
+input_start_time.addEventListener("input", () => {
+	input_duration.value = input_end_time.value - input_start_time.value;
+});
+input_end_time.addEventListener("input", () => {
+	input_duration.value = input_end_time.value - input_start_time.value;
+});
+input_start_time.addEventListener("change", () => {
+	if (cur_interval <= 0) {
+		input_start_time.value = 0;
+		input_duration.value = cur_end_time;
+		return;
+	}
+	if (input_start_time.value >= cur_end_time || input_start_time.value <= intervals[cur_interval - 1].start_time) {
+		input_start_time.value = cur_start_time;
+	}
+	cur_start_time = input_start_time.value;
+	cur_duration = cur_end_time - cur_start_time;
+	input_duration.value = cur_duration;
+});
+input_end_time.addEventListener("change", () => {
+	if (cur_interval >= intervals.length - 1) {
+		input_end_time.value = cur_end_time;
+		return;
+	}
+	if (input_end_time.value <= cur_start_time || input_end_time.value >= intervals[cur_interval + 1].end_time) {
+		input_end_time.value = cur_end_time;
+	}
+	cur_end_time = input_end_time.value;
+	cur_duration = cur_end_time - cur_start_time;
+	input_duration.value = cur_duration;
+});
+btn_save.addEventListener("click", () => {
+	if (cur_interval > 0) {
+		intervals[cur_interval].start_time = input_start_time.value;
+		intervals[cur_interval - 1].end_time = input_start_time.value;
+	}
+	intervals[cur_interval].end_time = input_end_time.value;
+	if (cur_interval < intervals.length - 1) {
+		intervals[cur_interval + 1].start_time = input_end_time.value;
+	}
+});
